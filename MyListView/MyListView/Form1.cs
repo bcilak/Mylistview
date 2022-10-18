c7b76dde979cb278b616de85e3639a211f23904b
bcilak <bcilak@gmail.com>
2022-10-18 16:49:26 +0300

    Mylistview
    Mylistview ile listview form elamanına benzer özellikte form elamanı oluşturuldu

---
+++ MyListView/MyListView/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("ADI", 100);
+            listView1.Columns.Add("SOYADI", 70);
+            listView1.Columns.Add("TELEFONU", 70);
+        }
+    }
+}
